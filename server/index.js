6fda567055ec7a7bcb65a41d18ced63f08607ddb
Shadan9113 <mdshadanzaheer@gmail.com>
2025-07-03 20:45:17 +0530

    Add user registration route with JWT token generation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require('cors')
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res)=>{
     res.send('API is running...')
