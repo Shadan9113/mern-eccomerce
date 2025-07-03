da1cf4afbefb4ba3b1d5bc46bc44f7ac8c8b9a89
Shadan9113 <mdshadanzaheer@gmail.com>
2025-07-03 15:23:45 +0530

    Add routes folder with productRoutes.js

---
+++ server/data/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    name: "Nike Air Max",
+    image: "/images/nike.jpg",
+    description: "High-performance running shoes",
+    brand: "Nike",
+    category: "Shoes",
+    price: 7999,
+    countInStock: 10,
+  },
+  {
+    name: "iPhone 14 Pro",
+    image: "/images/iphone.jpg",
+    description: "Latest Apple flagship phone",
+    brand: "Apple",
+    category: "Electronics",
+    price: 129999,
+    countInStock: 5,
+  },
+  {
+    name: "T-shirt Cotton Black",
+    image: "/images/tshirt.jpg",
+    description: "Soft and comfortable black T-shirt",
+    brand: "H&M",
+    category: "Clothing",
+    price: 699,
+    countInStock: 20,
+  },
+];
+
+module.exports = products;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors')
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes')
 
 
 dotenv.config();
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res)=>{
     res.send('API is running...')
